b471279fdfdeb52760cf30f4923132ff47d09de2
Samuel Cox <samc1253@gmail.com>
2023-04-12 11:15:39 -0600

    Add model validations and tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
+
+  validates_presence_of :first_name, :last_name, :email, :address
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :tea
   belongs_to :customer
+
+  validates_presence_of :title, :price, :status, :frequency
+
+  enum status: { active: 1, inactive: 2 }
 end
