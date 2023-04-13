1fea09be1d3392caa594ba8f0d4701fd6b2fa2a2
Samuel Cox <samc1253@gmail.com>
2023-04-13 14:59:42 -0600

    Add customer sad path testing

--- spec/requests/api/v1/customer_requests_spec.rb
+++ spec/requests/api/v1/customer_requests_spec.rb
@@ -26,5 +26,67 @@ RSpec.describe 'Customer Requests' do
       expect(created_customer.email).to eq(customer_params[:email])
       expect(created_customer.address).to eq(customer_params[:address])
     end
+
+    describe 'sad path/edge case testing' do
+      it 'will return an error if first name is not given' do
+        customer_params = {
+          last_name: Faker::Name.last_name,
+          email: Faker::Internet.email,
+          address: Faker::Address.full_address
+        }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        post api_v1_customers_path, headers: headers, params: JSON.generate(customer_params)
+
+        expect(response.status).to eq 422
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response[:errors].first).to eq("First name can't be blank")
+      end
+
+      it 'will return an error if last name is not given' do
+        customer_params = {
+          first_name: Faker::Name.first_name,
+          email: Faker::Internet.email,
+          address: Faker::Address.full_address
+        }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        post api_v1_customers_path, headers: headers, params: JSON.generate(customer_params)
+
+        expect(response.status).to eq 422
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response[:errors].first).to eq("Last name can't be blank")
+      end
+
+      it 'will return an error if email is not given' do
+        customer_params = {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          address: Faker::Address.full_address
+        }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        post api_v1_customers_path, headers: headers, params: JSON.generate(customer_params)
+
+        expect(response.status).to eq 422
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response[:errors].first).to eq("Email can't be blank")
+      end
+
+      it 'will return an error if address is not given' do
+        customer_params = {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          email: Faker::Internet.email
+        }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        post api_v1_customers_path, headers: headers, params: JSON.generate(customer_params)
+
+        expect(response.status).to eq 422
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response[:errors].first).to eq("Address can't be blank")
+      end
+    end
   end
 end
