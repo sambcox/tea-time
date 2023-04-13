e008d49a8e0f26c3188a6df9a334e7a14ef4543f
Samuel Cox <samc1253@gmail.com>
2023-04-13 14:48:23 -0600

    Add sad paths and edge cases to customer subscription requests

--- spec/requests/api/v1/customers/subscription_requests_spec.rb
+++ spec/requests/api/v1/customers/subscription_requests_spec.rb
@@ -18,5 +18,26 @@ RSpec.describe 'Customer Subscription Requests' do
         expect(subscription[:attributes][:customer_id]).to eq(customer.id)
       end
     end
+
+    describe 'sad path/edge case testing' do
+      it 'returns an error if the customer is not found' do
+        get api_v1_customer_subscriptions_path(999999)
+
+        expect(response.status).to eq 404
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response[:errors].first).to eq("Couldn't find Customer with 'id'=999999")
+      end
+
+      it 'returns an empty array if the customer has no subscriptions' do
+        customer = create(:customer)
+        get api_v1_customer_subscriptions_path(customer)
+
+        expect(response).to be_successful
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response[:data]).to eq([])
+      end
+    end
   end
 end
